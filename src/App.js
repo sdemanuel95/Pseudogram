3e83f7cfd198ff053090e755ce10df0bb26c1ce1
David Suarez <David_Suarez@intranet.itresources>
2019-09-24 09:37:04 -0300

    Voke la concha tu madre

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
+import React, { Component }
+  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import FileUpload from './FileUpload';
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({
+        user : user
+      });
+    });
+  }
+
+
+  handleAuth() {
+    debugger;
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code} : ${error.message}`));
+  }
+
+  handleLogout() {
+    firebase.auth().signOut()
+      .then(result => console.log(`${result.user.email} ha cerrado sesión`))
+      .catch(error => console.log(`Error ${error.code} : ${error.message}`));
+  }
+
+
+  renderLoginButton() {
+    //si el usuario esta logueado
+    if (this.state.user) {
+      return (
+        <div>
+          <p>Hola {this.state.user.displayName}</p>
+          <img src={this.state.user.photoURL} alt={this.state.user.displayName} />
+          <button onClick={this.handleLogout}>Salir</button>
+
+          <br/>
+          <FileUpload/>
+        </div>
+      );
+    } else {
+      return (
+        <button onClick={this.handleAuth}>Login con google</button>
+      )
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.renderLoginButton()}
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            If you don't jump, you are a volita.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDMG8Bpsq54_Qrj2ciSqaFV0TEy-mxGBxQ",
+    authDomain: "pseudogram-78c43.firebaseapp.com",
+    databaseURL: "https://pseudogram-78c43.firebaseio.com",
+    projectId: "pseudogram-78c43",
+    storageBucket: "gs://pseudogram-78c43.appspot.com",
+    messagingSenderId: "526832818394",
+    appId: "1:526832818394:web:8ec09999e0522db2"
+});
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
